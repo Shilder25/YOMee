efd1d8ad31eaccf990ba35c8ec427bb71398c9b1
Shilder25 <111251450+Shilder25@users.noreply.github.com>
2025-11-30 23:58:25 -0500

    Update HeroSection.tsx

--- client/src/components/HeroSection.tsx
+++ client/src/components/HeroSection.tsx
@@ -21,7 +21,7 @@ export default function HeroSection() {
           <Twitter className="h-8 w-8 md:h-10 md:w-10 lg:h-12 lg:w-12" />
         </a>
         <a
-          href="https://www.douyin.com/user/MS4wLjABAAAAGiLz1gg7IgM1EnvH4vaHt7R6o6ZKj7ljzSqyD2Z_8POEukCh9jynF3suM10QbKyl?from_tab_name=main&vid=7456017404477410600"
+          href="hhttps://www.douyin.com/search/yomee"
           target="_blank"
           rel="noopener noreferrer"
           className="w-16 h-16 md:w-20 md:h-20 lg:w-24 lg:h-24 rounded-lg bg-black text-white flex items-center justify-center hover-elevate active-elevate-2 transition-transform"
